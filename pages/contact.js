47d6d850a3152655b0a59e2650713f9c609e3b0e
Evan Edelstein <edelsteinevan@gmail.com>
2021-12-30 10:33:44 -0500

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -43,7 +43,7 @@ const Contact = () => {
             <Text fontWeight="bold">emial:</Text>
             <Text>e-mail: andras.fiser@einstein.yu.edu</Text>
         
-            <Box paddingY="16" alignItems="center">
+           <p align="center">
             <iframe
               display="block"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3017.91491721727!2d-73.84784458478687!3d40.85179177931703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2f579adfc39fd%3A0xeb70b73a47960c8c!2sPrice%20center!5e0!3m2!1sen!2sus!4v1636301787018!5m2!1sen!2sus"
@@ -55,7 +55,7 @@ const Contact = () => {
               aria-hidden="false"
               tabIndex="0"
             />
-            </Box>
+           </p>
             </VStack>
               </Flex>
               </Section>
